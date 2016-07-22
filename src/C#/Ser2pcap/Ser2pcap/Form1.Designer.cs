d481f508f38f2f91f85906a9342b555cf0759c42
Michael Zhang <michaelxmail@gmail.com>
2016-07-21 23:26:44 -0600

    Using C# to convert the file, almost work, still need to refine.

---
+++ src/C#/Ser2pcap/Ser2pcap/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Ser2pcap
+{
+    partial class FormSer2pcap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Combox_Pro_Sel = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Combox_Pro_Sel
+            // 
+            this.Combox_Pro_Sel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Combox_Pro_Sel.FormattingEnabled = true;
+            this.Combox_Pro_Sel.Items.AddRange(new object[] {
+            "DNP 3.0 @20000",
+            "IEC 104 @2404",
+            "IEC 61850  @102",
+            "Modbus TCP @502",
+            "IEC 101 @22401",
+            "IEC 103 @22403"});
+            this.Combox_Pro_Sel.Location = new System.Drawing.Point(37, 36);
+            this.Combox_Pro_Sel.Name = "Combox_Pro_Sel";
+            this.Combox_Pro_Sel.Size = new System.Drawing.Size(244, 23);
+            this.Combox_Pro_Sel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(37, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(172, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 183);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(325, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // FormSer2pcap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 205);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Combox_Pro_Sel);
+            this.Name = "FormSer2pcap";
+            this.Text = "Ser2pcap";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Combox_Pro_Sel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ src/C#/Ser2pcap/Ser2pcap/Form1.cs
@@ -0,0 +1,158 @@
+﻿/************************************
+This file convert serial communication traffic into Wireshark pcapng.
+Written by: Michael Zhang   michaelxmail[AT]gmail.com
+
+Input file format:
+12:34:56.789   Rx
+05 64 77 88 99 AA BB CC 
+12:34:56.790   Tx
+05 64 77 88 99 AA BB CC
+
+or
+[12:34:56.789]   Rx
+05 64 77 88 99 AA BB CC 
+[12:34:56.790]   Tx
+05 64 77 88 99 AA BB CC
+
+timestamp line with Rx or Tx
+data line
+
+If line has spaces at begining of the line, this tool will trim them.
+ 
+************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ser2pcap
+{
+    public partial class FormSer2pcap : Form
+    {
+        bool IsHex(char c)
+        {
+            return "0123456789ABCDEFabcdef".IndexOf(c) != -1;
+        }
+
+        public FormSer2pcap()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog SerialFileDialog = new OpenFileDialog();
+            SerialFileDialog.Title = "Select Serial traffic file";
+            SerialFileDialog.Filter = "TXT files|*.txt";
+            SerialFileDialog.InitialDirectory = @"C:\";
+            if (SerialFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                toolStripStatusLabel1.Text = SerialFileDialog.FileName;
+
+                string filename = SerialFileDialog.FileName;
+                string filepath = Path.GetDirectoryName(filename);
+                string newfilename = filepath+"\\"+Path.GetFileNameWithoutExtension(filename)+"_n.txt";
+                string pcapname = filepath + "\\" + Path.GetFileNameWithoutExtension(filename) + ".pcapng";
+                string[] filelines = File.ReadAllLines(filename);
+                int datacnt_pre= 0;
+                int datacnt = 0;
+
+                for (int cnt = 0; cnt < filelines.Length; cnt++)
+                {
+                    string tmpline = filelines[cnt].Trim();
+                    tmpline = tmpline.Trim('[');
+                    
+                    char char1 = 'x';
+                    char char2 = 'x';
+                    char char3 = 'x';
+
+                    if (tmpline.Length == 2)
+                    {
+                        char1 = tmpline[0];
+                        char2 = tmpline[1];
+
+                        //this line only has one data
+                        if((IsHex(char1) == true) & (IsHex(char2)==true))
+                        {
+                            tmpline = datacnt.ToString("X4") + " " + tmpline;
+                        }
+                        datacnt = datacnt + 1;
+                    }
+                    else if(tmpline.Length > 2)
+                    {
+                        char1 = tmpline[0];
+                        char2 = tmpline[1];
+                        char3 = tmpline[2];
+
+                        //timestamp line 12:34:56.789   
+                        if ((IsHex(char1) == true) & (IsHex(char2) == true) & (char3 == ':'))
+                        {
+                            datacnt = 0;
+                            if (tmpline.IndexOf("Rx") >= 0)      //has Rx in this line?
+                                tmpline = "I " + tmpline;
+                            else if (tmpline.IndexOf("Tx") >= 0)  //has Tx in this line?
+                                tmpline = "O " + tmpline;
+                        }
+                        //data line
+                        else if((IsHex(char1) == true) & (IsHex(char2) == true) & (char3 == ' '))
+                        {
+                            //count the data
+                            string[] splitLines = tmpline.Split(' ');
+                            datacnt_pre = datacnt;
+
+                            foreach(string dstr in splitLines)
+                            {
+                                if (dstr.Length == 2)
+                                    datacnt++;
+                            }
+
+                            tmpline = datacnt_pre.ToString("X4") + " " + tmpline;
+                            //then add offset at begining
+                        }
+                        else
+                        {
+                            tmpline = '#' + tmpline;
+                        }
+                            //MessageBox.Show(tmpline + ";" + Convert.ToString(tmpline.Length) + ";" + char1 + char2 + char3);
+                    }
+                    else    //empty line
+                    {
+                        tmpline = '#' + tmpline;
+                    }
+                    filelines[cnt] = tmpline;
+                }
+                File.WriteAllLines(newfilename, filelines);
+
+                //call wireshark text2pcap to convert the txt file
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = false;
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "C:\\Program Files\\Wireshark\\text2pcap.exe";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                //add " around new file name, in case file name has space and will cause text2pcap execute wrong
+                startInfo.Arguments = "-D -T 1111,22403 -t %H:%M:%S. "+"\""+newfilename + "\"" + " "+ "\""+pcapname + "\"";
+
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(startInfo))
+                    {
+                        exeProcess.WaitForExit();
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                }
+            }
+        }
+    }
+}
